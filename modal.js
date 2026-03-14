6bb41088e6997e519f0c0a4a8f2342cb5ac4bc0a
Maksim_Marchuk <makstop280409@gmail.com>
2026-03-14 12:44:00 +0200

    asdasdasd

--- modal.js
+++ modal.js
@@ -13,3 +13,25 @@
         document.body.classList.toggle("no-scroll");
     }
 })();
+
+
+const dataList = JSON.parse(localStorage.getItem("dataSession")) || [];
+
+const saveData = (event) => {
+    event.preventDefault();
+
+    const data = {
+        name: document.getElementById("strName").value,
+        telephone: document.getElementById("strTel").value,
+        date: document.getElementById("strDate").value,
+        travel: document.getElementById("strIsland").value
+    }
+
+    dataList.push(data);
+    localStorage.setItem("dataSession", JSON.stringify(dataList));
+    console.log(data)
+    event.target.reset();
+}
+
+
+document.querySelector("form").addEventListener("submit", saveData);
